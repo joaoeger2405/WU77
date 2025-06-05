b7c7eddcc9ae308e63cf4c0da75966e776c88974
joaoeger2405 <jv838137@gmail.com>
2025-06-05 20:25:15 -0300

    Update teste indentaçao.py

--- teste indentaçao.py
+++ teste indentaçao.py
@@ -1,9 +1,11 @@
 if 5>10:
 print("eu vou ali jaja")
 
-
+essa é um projeto indetaçao 
 if 5>2:
 print("aqui nao é o local correto")
+
+essa é um projeto indetaçao 
 
 if 10>11:
     print("estou com fome")
