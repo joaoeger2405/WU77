0ec13ba1cfcd1199faebbaf8e75e3bb411a0b3a7
joaoeger2405 <jv838137@gmail.com>
2025-06-05 19:59:32 -0300

    meu primeiro commit
    estou versionando

---
+++ WU77Joao.py
@@ -0,0 +1,4 @@
+print("livro")
+print("uma noite sem samuel")
+print("descrição")
+print("esse livro relata um dia sem o professor samuel que esta de atestado, e a usinagem foi para TI")
---
+++ teste indentaçao.py
@@ -0,0 +1,9 @@
+if 5>10:
+print("eu vou ali jaja")
+
+
+
+print("aqui nao é o local correto")
+
+
+
